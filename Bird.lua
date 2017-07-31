0800d30edda54ef617173a37f7211bab2697e33b
Brooks Patton <scgryphon@sbcglobal.net>
2017-07-31 16:35:56 -0600

    the bird now falls

--- Bird.lua
+++ Bird.lua
@@ -10,6 +10,8 @@ function Bird.new(location)
   t.location = location
   t.size = 15
   t.color = {255, 0, 0}
+  t.acceleration = Vector.new(0, 0)
+  t.velocity = Vector.new(0, 0)
 
   return t
 end
@@ -17,6 +19,16 @@ end
 function Bird:draw()
   love.graphics.setColor(self.color)
   love.graphics.rectangle('fill', self.location.x, self.location.y, self.size, self.size)
+end
+
+function Bird:applyForce(force)
+  self.acceleration = self.acceleration + force
+end
+
+function Bird:update()
+  self.velocity = self.velocity + self.acceleration
+  self.location = self.location + self.velocity
+  self.acceleration = self.acceleration * 0
 end
 
 return Bird
--- main.lua
+++ main.lua
@@ -1,12 +1,14 @@
 local Bird = require('./Bird')
 local Vector = require('./vector')
 local bird
+local gravity
 
 function love.load()
   width = love.graphics.getWidth()
   height = love.graphics.getHeight()
   local location = Vector.new(25, 25)
   bird = Bird.new(location)
+  gravity = Vector.new(0, 5)
 end
 
 function love.draw()
@@ -14,4 +16,8 @@ function love.draw()
 end
 
 function love.update(dt)
+  if dt > 0.029 then dt = 0.029 end
+
+  bird:applyForce(gravity * dt)
+  bird:update()
 end
